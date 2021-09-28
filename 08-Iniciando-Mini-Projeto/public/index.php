56b0ef93a4aab96c7ffc3fc6f11302a355ba19b2
José Malcher Jr <contato@josemalcher.net>
2021-09-28 08:57:51 -0300

    73 - Verificando Parâmetros da URL

--- 08-Iniciando-Mini-Projeto/public/index.php
+++ 08-Iniciando-Mini-Projeto/public/index.php
@@ -1,3 +1,24 @@
 <?php
 
-echo __FILE__;
+// Query String ou QUery Params ou Parâmetros de URL
+
+// localhost:3030?nome=Jose
+// [name => Jose]
+
+// localhost:3030?name=jose&email=jose@jose.com&idade=37
+// [name => 'jose', email => jose@jose.com, idade => 30
+
+//var_dump($_GET);
+//die;
+
+// Esta URL será o valor para a navegação
+$pagina = isset($_GET['pagina']) ? $_GET['pagina'] : '/';
+
+if ($pagina == '/') {
+    echo 'Página inicial';
+}
+
+if ($pagina == '/produto') {
+    echo 'Detalhe do Produto';
+}
+
