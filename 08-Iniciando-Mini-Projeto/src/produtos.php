02f831007ffab9c472caab254403d135ad5a8b23
José Malcher Jr <contato@josemalcher.net>
2021-09-28 20:32:27 -0300

    77 - PHP & HTML Juntos

--- 08-Iniciando-Mini-Projeto/src/produtos.php
+++ 08-Iniciando-Mini-Projeto/src/produtos.php
@@ -4,16 +4,16 @@ return [
     [
         'codigo' => '0001',
         'nome' => 'Produto Teste',
-        'preço' => 'R$ 19,90'
+        'preco' => 'R$ 19,90'
     ],
     [
         'codigo' => '0002',
         'nome' => 'Produto Teste 2',
-        'preço' => 'R$ 109,90'
+        'preco' => 'R$ 109,90'
     ],
     [
         'codigo' => '0003',
         'nome' => 'Produto Teste 3',
-        'preço' => 'R$ 99,90'
+        'preco' => 'R$ 99,90'
     ]
 ];
--- 08-Iniciando-Mini-Projeto/templates/lista.php
+++ 08-Iniciando-Mini-Projeto/templates/lista.php
@@ -1,3 +1,15 @@
-<pre>
-<?php
-var_dump(PRODUTOS);
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Listagem de Produtos</title>
+</head>
+<body>
+    <?php foreach (PRODUTOS as $produto) : ?>
+        <?php echo $produto['nome'] ?> <?php echo $produto['preco'] ?> <br>
+    <?php endforeach; ?>
+</body>
+</html>
