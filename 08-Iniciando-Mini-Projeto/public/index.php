38facf5b3c5418da68c49cbdd433868114ad335e
José Malcher Jr <contato@josemalcher.net>
2022-01-19 19:36:28 -0300

    87 - Submentendo Formulário

--- 08-Iniciando-Mini-Projeto/public/index.php
+++ 08-Iniciando-Mini-Projeto/public/index.php
@@ -55,3 +55,6 @@ if ($pagina == '/produto') {
 if ($pagina == '/produto/criar') {
     require TEMPLATES . '/criar-produto.phtml';
 }
+if ($pagina == '/produto/salvar') {
+    var_dump($_POST); // array(4) { ["nome"]=> string(0) "" ["descricao"]=> string(0) "" ["sobre"]=> string(8) " " ["valor"]=> string(0) "" }
+}
