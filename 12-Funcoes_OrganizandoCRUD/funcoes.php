a1ced5b2bf740f6b6c5a67dca53034612da9b0d4
José Malcher Jr <contato@josemalcher.net>
2022-06-02 17:21:05 -0300

    123 - Variadic Functions

--- 12-Funcoes_OrganizandoCRUD/funcoes.php
+++ 12-Funcoes_OrganizandoCRUD/funcoes.php
@@ -10,7 +10,38 @@ ola();*/
 
 function olaUsuario(string $nome, string $sobrenome)
 {
+
+    // var_dump(func_get_args());
     echo 'Olá ' . $nome . $sobrenome . ', tudo certo?';
 }
 
-olaUsuario('Jose', 'Malcher jr');
+// olaUsuario('Jose', 'Malcher jr');
+
+function nomes($empresas, ...$nomes)
+{
+    var_dump($nomes);
+    var_dump(func_get_args());
+
+}
+nomes("joise", "maria", "Cassandra");
+
+/*
+ $ php funcoes.php
+array(2) {
+  [0]=>
+  string(5) "maria"
+  [1]=>
+  string(9) "Cassandra"
+}
+array(3) {
+  [0]=>
+  string(5) "joise"
+  [1]=>
+  string(5) "maria"
+  [2]=>
+  string(9) "Cassandra"
+}
+
+
+ */
+
