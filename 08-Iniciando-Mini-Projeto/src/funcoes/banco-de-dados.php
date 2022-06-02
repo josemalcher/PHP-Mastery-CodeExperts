72ab5ed5d14c568cd0f7a96f46cee0fc65a1d6a6
José Malcher Jr <contato@josemalcher.net>
2022-06-02 18:35:35 -0300

    127 - Função Criar Dados

--- 08-Iniciando-Mini-Projeto/src/funcoes/banco-de-dados.php
+++ 08-Iniciando-Mini-Projeto/src/funcoes/banco-de-dados.php
@@ -21,10 +21,31 @@ function selectWhere($tabela, $id, $colunas = '*')
     return $select->fetch(PDO::FETCH_ASSOC);
 }
 
-function criar()
+function criar($tabela, $dados)
 {
 
+    $keys = array_keys($dados);
+    $colunas = implode(', ', $keys);
+    $binds = implode(', :', $keys);
+
+    $sql = 'INSERT INTO ' . $tabela . '(' . $colunas . ') VALUES(:' . $binds . ')';
+                /*'(nome, descricao, sobre, preco, status, criacao_em, atualizacao_em)
+        VALUES()';*/
+
+    $inserir = $conexao->prepare($sql);
+
+    foreach ($dados as $key => $valor) {
+        $inserir->bindValue(':' . $key, $valor, is_integer($valor) ?
+                                                PDO::PARAM_INT :
+                                                PDO::PARAM_STR);
+    }
+    return $inserir->execute();
+
+    // echo $sql;
 }
+
+// criar('produtos', ['nome'=>'Mouse', 'preco' => 19.9, 'descricao'=>'descrição do produto Mouse']);
+
 
 function atualizar()
 {
