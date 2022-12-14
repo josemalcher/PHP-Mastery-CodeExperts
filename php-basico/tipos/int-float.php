1fd0fc9cfc7c12e22548a75aa2b8d90d348196f5
Jose Malcher Jr <contato@josemalcher.net>
2022-12-14 08:43:51 -0300

    24 - Inteiros / Ponto Flutuante

--- php-basico/tipos/int-float.php
+++ php-basico/tipos/int-float.php
@@ -9,7 +9,7 @@ echo "<br>";
 
 //Sistema Octal no PHP
 //Prefixado 0
-echo 0777; //retorna 511
+echo 0777; //retorna 511 
 
 echo "<br>";
 //Sistema Hexadecimal
@@ -26,17 +26,17 @@ echo 0.90;
 echo "<br>";
 
 echo "<br>";
-/*apesar de retornar 1, por conta do .
+/*apesar de retornar 1, por conta do . 
 o php entende como float
 */
-echo 1.0;
+echo 1.0; 
 //Retorna o tipo do dados informado no gettype
 echo gettype(1.0);
 
 echo "<br>";
 
 /*
-Contantes tamanho minimo e maximo
+Contantes tamanho minimo e maximo 
 suportado pelo PHP para tipo inteiro
 */
 echo PHP_INT_MIN;
@@ -47,7 +47,7 @@ echo "<br>";
 echo "<br>";
 
 /*
-Contantes tamanho minimo e maximo
+Contantes tamanho minimo e maximo 
 suportado pelo PHP para tipo float
 */
 echo PHP_FLOAT_MIN;
@@ -56,11 +56,11 @@ echo PHP_FLOAT_MAX;
 
 echo "<br>";
 
-//Exponencial: 13.3 (E)levado a 10 +2
+//Exponencial: 13.3 (E)levado a 10 +2 
 //(a virgula se movimenta 2 casas para a direita por conta do +2)
 echo 13.3e+2; //Res.: 1300.00
 echo "<br>";
 
-//Exponencial: 13.3 (E)levado a 10 -3
+//Exponencial: 13.3 (E)levado a 10 -3 
 //(a virgula se movimenta 3 casas para a esquerda por conta do -3)
 echo 13.3e-3; // Res.: 0.0133
